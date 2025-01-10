0ac9f2cbfeb98f0dd56d9e99495b098d669b861f
ShrieSannith <shriesannith.n2021@vitstudent.ac.in>
2025-01-10 13:39:00 +0530

    deploy

--- src/configurations/dbConfig.js
+++ src/configurations/dbConfig.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://user1:user1@cms.r2k2apx.mongodb.net/?retryWrites=true&w=majority&appName=CMS");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://user10:user10@cms.arxmh0w.mongodb.net/?retryWrites=true&w=majority&appName=CMS"
+);
 mongoose.connection.on("connected", () => {
-    console.log("Connected");
-}
-)
+  console.log("Connected");
+});
 mongoose.connection.on("error", (err) => {
-    console.log(`${err}`);
-}
-)
+  console.log(`${err}`);
+});
 
 module.exports = mongoose;
